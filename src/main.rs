f7f4e28a0980280afa97234e6616363df11d2452
潘岩 <1227818703@qq.com>
2026-03-11 17:58:56 +0800

    根据type返回指定类型的响应，如果没有则返回NXDOMAIN及SOA记录

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::{io, net::SocketAddr};
+use std::collections::HashMap;
 use tokio::io::AsyncReadExt;
 use tokio::net::{TcpListener, UdpSocket};
 use crate::codec::{DnsDecoder, DnsEncoder};
-use crate::dns_packet::{DnsPacket, DnsQuestion, DnsRecordA, DnsRecordAAAA, DnsRecordCNAME, DnsRecordMX, DnsRecordNS, DnsRecordSOA, Flags, Opcode, PacketBuilder, Rcode};
+use crate::dns_packet::{DnsPacket, DnsPacketRef, DnsQuestion, DnsRecord, DnsRecordA, DnsRecordAAAA, DnsRecordCNAME, DnsRecordMX, DnsRecordNS, DnsRecordSOA, Flags, Opcode, PacketBuilder, Rcode};
 // use tokio::sync::mpsc;
 
 mod controller;
@@ -58,6 +59,7 @@ async fn handle_tcp(tcp_listener: &TcpListener) -> io::Result<()> {
 }
 
 async fn handle_udp(udp_socket: &UdpSocket) -> Result<(), anyhow::Error> {
+    let mut store = record_store().await;
     loop {
         let decoder = DnsDecoder::new();
         let mut buf = [0; 4096];
@@ -67,72 +69,78 @@ async fn handle_udp(udp_socket: &UdpSocket) -> Result<(), anyhow::Error> {
         let domain_name = question.domain_name.as_str();
         println!("{}", domain_name);
 
-        let encoder = DnsEncoder::<&mut DnsPacket>::new();
-        let data = encoder.encode(&mut create_packet(packet.header.id, domain_name, question.q_type).await);
+        let encoder = DnsEncoder::<DnsPacketRef>::new();
+        let mut builder = create_packet(packet.header.id, &question).await;
+        let data = store.get(&question.q_type);
+        let packet = match data {
+            Some(data) => {
+                for answer in data {
+                    builder.add_answer(answer);
+                }
+                builder.build()
+            },
+            None => {
+                if let Some(soa_records) = store.get(&0x0006) {
+                    for soa in soa_records {
+                        builder.add_authority(soa);
+                    }
+                }
+                let mut p = builder.build();
+                p.header.flags.rc = Rcode::NXDOMAIN;
+                p
+            }
+        };
+
+        let data = encoder.encode(packet);
         let d = data.as_slice();
         let _ = udp_socket.send_to(d, addr).await?;
     }
 }
 
-async fn create_packet(id: u16, domain_name: &str, q_type: u16) -> DnsPacket {
-    let mut builder = PacketBuilder::<DnsPacket>::default();
-    builder.id(id);
-    let flags = Flags {
-        qr: true,
-        oc: Opcode::QUERY,
-        aa: true,
-        tc: false,
-        rd: true,
-        ra: true,
-        z: 0,
-        rc: Rcode::NOERROR,
-    };
-    builder.flag(flags);
-    let question = DnsQuestion {
-        domain_name: domain_name.to_string(),
-        q_type,
-        q_class: 0x0001,
-    };
-    builder.add_question(question);
-
+async fn record_store() -> HashMap<u16, Vec<DnsRecord>> {
+    let mut map = HashMap::new();
     let record1 = DnsRecordA {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x0001,
         ttl: 600,
         length: 4,
         data: [124, 16, 31, 99]
     };
-    builder.add_answer(record1.into());
+    map.entry(record1.record_type).or_insert(Vec::new()).push(record1.into());
+
     let record2 = DnsRecordA {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x0001,
         ttl: 600,
         length: 4,
         data: [124, 16, 31, 100]
     };
-    builder.add_answer(dns_packet::DnsRecord::from(record2));
+    map.entry(record2.record_type).or_insert(Vec::new()).push(record2.into());
+
     let record3 = DnsRecordAAAA {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x001c,
         ttl: 600,
         length: 16,
         data: [0x20, 0x01, 0x0d, 0xb8, 0x85, 0xa3, 0x00, 0x00, 0x00, 0x00, 0x8a, 0x2e, 0x03, 0x70, 0x73, 0x34]
     };
-    builder.add_answer(record3.into());
+    map.entry(record3.record_type).or_insert(Vec::new()).push(record3.into());
+
     let record4 = DnsRecordCNAME {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x0005,
         ttl: 600,
         length: 0,
         data: "test.guokeyun.com".to_string(),
     };
-    builder.add_answer(record4.into());
+    map.entry(record4.record_type).or_insert(Vec::new()).push(record4.into());
+
     let record5 = DnsRecordMX {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x000f,
         ttl: 600,
@@ -140,9 +148,10 @@ async fn create_packet(id: u16, domain_name: &str, q_type: u16) -> DnsPacket {
         preference: 10,
         exchange: "mail.guokeyun.com".to_string(),
     };
-    builder.add_answer(record5.into());
+    map.entry(record5.record_type).or_insert(Vec::new()).push(record5.into());
+
     let record6 = DnsRecordMX {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x000f,
         ttl: 600,
@@ -150,18 +159,20 @@ async fn create_packet(id: u16, domain_name: &str, q_type: u16) -> DnsPacket {
         preference: 20,
         exchange: "124.16.31.56".to_string(),
     };
-    builder.add_answer(record6.into());
+    map.entry(record6.record_type).or_insert(Vec::new()).push(record6.into());
+
     let record7 = DnsRecordNS {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x0002,
         ttl: 600,
         length: 0,
         data: "ns1.sfn.cn".to_string(),
     };
-    builder.add_answer(record7.into());
+    map.entry(record7.record_type).or_insert(Vec::new()).push(record7.into());
+
     let record8 = DnsRecordSOA {
-        domain_name: domain_name.to_string(),
+        domain_name: "www.guokeyun.com".to_string(),
         record_class: 1,
         record_type: 0x0006,
         ttl: 600,
@@ -174,6 +185,25 @@ async fn create_packet(id: u16, domain_name: &str, q_type: u16) -> DnsPacket {
         expire: 10,
         minimum: 600,
     };
-    builder.add_answer(record8.into());
-    builder.build()
+    map.entry(record8.record_type).or_insert(Vec::new()).push(record8.into());
+
+    map
+}
+
+async fn create_packet<'a>(id: u16, question: &'a DnsQuestion) -> PacketBuilder<DnsPacketRef<'a>> {
+    let mut builder = PacketBuilder::<DnsPacketRef>::default();
+    builder.id(id);
+    let flags = Flags {
+        qr: true,
+        oc: Opcode::QUERY,
+        aa: true,
+        tc: false,
+        rd: true,
+        ra: true,
+        z: 0,
+        rc: Rcode::NOERROR,
+    };
+    builder.flag(flags);
+    builder.add_question(question);
+    builder
 }
